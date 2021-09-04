22c55655f9b26c93e30aebce8d202eab4e506e79
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-09-04 07:59:47 +0300

    add webpack build config

---
+++ packages/server/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+  },
+}
