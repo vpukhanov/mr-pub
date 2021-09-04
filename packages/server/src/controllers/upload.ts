d7f95188734c9afd28c70987966dd801861aa0d5
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-09-04 16:33:26 +0300

    upload file and return uuid

--- packages/server/src/controllers/upload.ts
+++ packages/server/src/controllers/upload.ts
@@ -1,5 +1,9 @@
 import { Express } from 'express'
 import parseDiff from 'parse-diff'
+import { Storage } from '@google-cloud/storage'
+import { v4 as uuidv4 } from 'uuid'
+
+const STORAGE = new Storage()
 
 export function getDiffFromFile(file: Express.Multer.File) {
   if (!file) {
@@ -12,4 +16,10 @@ export function getDiffFromFile(file: Express.Multer.File) {
     )
   }
   return diff
+}
+
+export async function uploadFile(file: Express.Multer.File) {
+  const fileName = uuidv4()
+  await STORAGE.bucket('ether.mister.pub').file(fileName).save(file.buffer)
+  return fileName
 }
--- packages/server/src/routes/upload-diff.ts
+++ packages/server/src/routes/upload-diff.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express'
-import { getDiffFromFile } from '../controllers/upload'
+import { getDiffFromFile, uploadFile } from '../controllers/upload'
 import { upload } from '../middleware/multer'
 
 export function configureUploadDiffRoute(app: Express) {
-  app.post('/upload-diff', upload.single('diff'), (req, res) => {
-    const diff = getDiffFromFile(req.file)
-    res.status(200).json(diff)
+  app.post('/upload-diff', upload.single('diff'), async ({ file }, res) => {
+    getDiffFromFile(file) // call this for validation purposes
+    const id = await uploadFile(file)
+    res.status(200).end(id)
   })
 }
