b08874411875fbeb8886ca90729dd3775c8cca82
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-09-04 18:17:28 +0300

    adds terraform kubernetes configuration

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,19 @@
+resource "google_container_cluster" "default" {
+  name     = "default-cluster"
+  location = "europe-north1-a"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "default_preemptible_nodes" {
+  name       = "default-cluster-preemptible-pool"
+  location   = google_container_cluster.default.location
+  cluster    = google_container_cluster.default.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+  }
+}
