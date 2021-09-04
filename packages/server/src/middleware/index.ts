c362cf65c7fcc1ff0cf0e66833f60ccc21f061a5
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-09-04 16:37:14 +0300

    delete middleware index

--- packages/server/src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-import { Express } from 'express'
-import { configureMulter } from './multer'
-
-export function configureMiddleware(app: Express) {
-  configureMulter(app)
-}
--- packages/server/src/middleware/multer.ts
+++ packages/server/src/middleware/multer.ts
@@ -1,8 +1,3 @@
-import { Express } from 'express'
 import multer from 'multer'
 
 export const upload = multer({ storage: multer.memoryStorage() })
-
-export function configureMulter(app: Express) {
-  // no configuration required for this middleware
-}
