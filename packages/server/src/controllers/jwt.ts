682308fad4aae30078c7b1c2bd5141fa94930f72
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-09-12 08:18:49 +0300

    feat(server): adds jwt controller methods

---
+++ packages/server/src/controllers/jwt.ts
@@ -0,0 +1,55 @@
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret'
+
+type TokenPayload = {
+  owned: string[]
+}
+
+async function decodeToken(token: string) {
+  return new Promise<TokenPayload>((resolve, reject) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded: TokenPayload) => {
+      if (!err && decoded) {
+        resolve(decoded)
+      } else {
+        reject()
+      }
+    })
+  })
+}
+
+export async function tokenOwnsId(token: string, id: string) {
+  try {
+    const decoded = await decodeToken(token)
+    return decoded.owned.includes(id)
+  } catch (e) {
+    return false
+  }
+}
+
+export async function createOwnerToken(id: string, previousToken?: string) {
+  let owned = []
+
+  try {
+    const decoded = await decodeToken(previousToken)
+    owned = decoded.owned
+  } catch (e) {
+    // Ignore previous invalid token
+  }
+
+  owned.push(id)
+
+  return new Promise<string>((resolve, reject) => {
+    jwt.sign(
+      { owned },
+      JWT_SECRET,
+      (err: Error | null, encoded: string | undefined) => {
+        if (!err && encoded) {
+          resolve(encoded)
+        } else {
+          reject()
+        }
+      },
+    )
+  })
+}
